1d7e5e301314afd039ed7e84333b18f07df4d27b
lei.wang <316434776@qq.com>
2023-10-16 10:30:26 +0800

    1、fix：update main configfile file.yaml->fms.yaml

--- fms.go
+++ fms.go
@@ -35,7 +35,7 @@ import (
 	"github.com/zeromicro/go-zero/rest"
 )
 
-var configFile = flag.String("f", "etc/file.yaml", "the config file")
+var configFile = flag.String("f", "etc/fms.yaml", "the config file")
 
 func main() {
 	flag.Parse()
