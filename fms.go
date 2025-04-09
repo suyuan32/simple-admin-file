09038ee97d0222f366127453cef78e488345e759
Ryan Su <yuansu.china.work@gmail.com>
2025-04-09 16:30:12 +0800

    chore: upgrade deps

--- fms.go
+++ fms.go
@@ -5,7 +5,7 @@
 //		Schemes: http, https
 //		Host: localhost:9102
 //		BasePath: /
-//		Version: 1.6.5
+//		Version: 1.6.6
 //		Contact: yuansu.china.work@gmail.com
 //		securityDefinitions:
 //		  Token:
