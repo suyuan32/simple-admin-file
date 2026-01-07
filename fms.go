550a208a3346c25c6b11b2662abee1cc27c63faa
Ryan SU <yuansu.china.work@gmail.com>
2026-01-07 15:54:41 +0800

    chore: upgrade version

--- fms.go
+++ fms.go
@@ -5,7 +5,7 @@
 //		Schemes: http, https
 //		Host: localhost:9102
 //		BasePath: /
-//		Version: 1.7.5
+//		Version: 1.7.9
 //		Contact: yuansu.china.work@gmail.com
 //		securityDefinitions:
 //		  Token:
